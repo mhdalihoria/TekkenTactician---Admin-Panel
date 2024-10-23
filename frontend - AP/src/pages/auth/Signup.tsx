4f31517bece935c5d174a4cb6c2bf868171276eb
mhdalihoria <alihoria123456@gmail.com>
2024-10-23 21:37:27 +0300

    Adding Action Function (posts requests with react router dom 6.4)

--- frontend - AP/src/pages/auth/Signup.tsx
+++ frontend - AP/src/pages/auth/Signup.tsx
@@ -1,9 +1,11 @@
 import { TextField } from "@mui/material";
 import { CButton } from "../../custom-components/CButton";
+import { ActionFunctionArgs, Form, redirect } from "react-router-dom";
+import { RegisterUser } from "../../api/auth/postRequests";
 
 export default function Signup() {
   return (
-    <div>
+    <Form method="post" action="/signup">
       <TextField name="username" label="Username" variant="outlined" />
       <TextField name="email" label="Email" type="email" variant="outlined" />
       <TextField
@@ -13,13 +15,27 @@ export default function Signup() {
         variant="outlined"
       />
 
-      <CButton
-        color="secondary"
-        btnSize="md"
-        onClick={() => console.log("it works")}
-      >
+      <CButton color="primary" btnSize="md" type="submit">
         Signup
       </CButton>
-    </div>
+    </Form>
   );
 }
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const username = formData.get("username") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const result = await RegisterUser({ username, email, password });
+
+  if (result.success) {
+    console.log(result);
+    return redirect(`/`);
+
+  } else {
+    console.log(result);
+    return null;
+  }
+};
