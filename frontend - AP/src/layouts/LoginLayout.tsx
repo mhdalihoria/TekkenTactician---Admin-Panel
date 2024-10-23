66036d7e27ed340ba1301c823b3679b25d5e4b35
mhdalihoria <alihoria123456@gmail.com>
2024-10-23 20:26:04 +0300

    Baisc Layout Designs

--- frontend - AP/src/layouts/LoginLayout.tsx
+++ frontend - AP/src/layouts/LoginLayout.tsx
@@ -1,16 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Box, styled } from "@mui/material";
+import { Outlet, useLocation } from "react-router-dom";
 
-const styles = {
-  background: "lightblue",
+const LoginLayoutContainer = styled(Box)(({ theme }) => ({
+  background: theme.palette.primary.main,
+  minHeight: "100vh",
+  minWidth: "100vw",
+
   display: "flex",
+  flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-};
+}));
+
+const AuthPageTitle = styled("h1")(({ theme }) => ({
+  fontFamily: "Tarrget",
+  marginBottom: "5.625rem",
+  fontSize: "48px",
+  color: theme.palette.accent.light,
+}));
 
 export default function LoginLayout() {
+  const location = useLocation();
+  const pageTitle = location.pathname.split("/")[1];
+
   return (
-    <div style={styles}>
+    <LoginLayoutContainer>
+      <AuthPageTitle>{pageTitle}</AuthPageTitle>
       <Outlet />
-    </div>
+    </LoginLayoutContainer>
   );
 }
