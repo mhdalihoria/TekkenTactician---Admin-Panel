ab08d719bdd84b878ff1512caec3dbe69c793957
mhdalihoria <alihoria123456@gmail.com>
2024-10-23 20:27:22 +0300

    Creating MUI Theme

--- frontend - AP/src/main.tsx
+++ frontend - AP/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme.ts"
+import theme from "./theme/theme.ts"
 
 
 createRoot(document.getElementById("root")!).render(
---
+++ frontend - AP/src/theme/theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material/styles";
+import { themeColors } from "./themeColors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0e223f",
+      light: "#193e73",
+      dark: "#193e73",
+    },
+    secondary: {
+      main: "#3559c2",
+      light: "#3559C2",
+      dark: "#253e87",
+    },
+    accent: {
+      main: "#088395",
+      light: "#399BAA",
+      dark: "#055B68",
+    },
+    ...themeColors
+  },
+});
+
+export default theme;
