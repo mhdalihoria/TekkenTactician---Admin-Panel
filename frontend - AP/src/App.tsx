6ddb6948f070df162a36147edf5da98948877ec5
mhdalihoria <alihoria123456@gmail.com>
2024-10-23 21:35:47 +0300

    Adding new route (with seperate layouts)

--- frontend - AP/src/App.tsx
+++ frontend - AP/src/App.tsx
@@ -5,17 +5,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/auth/Login";
 import LoginLayout from "./layouts/LoginLayout";
-import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
 import { action as LoginAction } from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import { action as SignupAction } from "./pages/auth/Signup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<LoginLayout />}>
-      <Route path="login" element={<Login />} action={LoginAction} />
-      <Route path="signup" element={<Signup />} />
-    </Route>
+    <>
+      <Route element={<LoginLayout />}>
+        <Route path="login" element={<Login />} action={LoginAction} />
+        <Route path="signup" element={<Signup />} action={SignupAction} />
+      </Route>
+      <Route element={<>main app layout</>}>
+        <Route path="/" element={<h1>main page</h1>} />
+      </Route>
+    </>
   )
 );
 
