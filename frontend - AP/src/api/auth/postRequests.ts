daad3adeeef656eb04df5a3c45d14f78b0b9156e
mhdalihoria <alihoria123456@gmail.com>
2024-10-23 21:36:46 +0300

    Adding Signup fetch request

---
+++ frontend - AP/src/api/auth/postRequests.ts
@@ -0,0 +1,58 @@
+export const SubmitPost = async ({
+  name,
+  email,
+}: {
+  name: string;
+  email: string;
+}) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/auth/login`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const RegisterUser = async ({
+  username,
+  email,
+  password,
+}: {
+  username: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/auth/signup`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+};
