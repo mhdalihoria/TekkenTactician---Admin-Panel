0b25e149702a4a1c4751d6cab904d212a1f98d05
mhdalihoria <alihoria123456@gmail.com>
2024-10-24 09:53:52 +0300

    Added Utlity Function to take in Colors from MUI theme

--- frontend - AP/src/custom-components/CInputField.tsx
+++ frontend - AP/src/custom-components/CInputField.tsx
@@ -1,4 +1,4 @@
-import { styled, TextField, TextFieldProps } from "@mui/material";
+import { Palette, styled, TextField, TextFieldProps } from "@mui/material";
 
 type CustomInputFieldTypes = TextFieldProps & {
   fieldColor?: string;
@@ -6,82 +6,102 @@ type CustomInputFieldTypes = TextFieldProps & {
 
 const CustomInputField = styled(TextField, {
   shouldForwardProp: (prop) => prop !== "fieldColor",
-})<{ fieldColor: string }>(({ theme, fieldColor }) => ({
-  /*
-    * .MuiInputLabel-root => style of the label of the text field.
-    * .MuiInputLabel-root.Mui-focused => Controls the label's color when the input field is focused.
-    * .MuiInputBase-input => Controls the color and style of the actual text entered inside the input field.
-    * .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline => Controls the outline color and style when the text field is in its normal state.
-    * .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline => Controls the outline color when the input field is focused.
-    * .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline => Controls the outline color when hovering over the input field.    
-    * .MuiInputBase-input::placeholder => Controls the style and color of the placeholder text inside the input field.
-    * .MuiFormHelperText-root => Controls the color and style of the helper text displayed below the input field.
-    * .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline => Controls the outline color when the input field has an error.
-    * .MuiInputLabel-root.Mui-error => Controls the color of the label when the input field is in an error state.
-    * .MuiFormHelperText-root.Mui-error => Controls the color of the helper text when there's an error.
-    * .MuiInputBase-root.Mui-disabled => Controls the styles when the input is disabled.
-    
-    */
+})<{ fieldColor: string }>(({ theme, fieldColor }) => {
 
-// first way of doing it, more compact
-  //   ".MuiInputLabel-root, .MuiInputLabel-root.Mui-focused, .MuiInputBase-input, .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline, .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline, .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline, .MuiInputBase-input::placeholder, .MuiFormHelperText-root, .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline, .MuiInputLabel-root.Mui-error, .MuiFormHelperText-root.Mui-error, .MuiInputBase-root.Mui-disabled,.MuiInputAdornment-root, .MuiOutlinedInput-root.Mui-disabled .MuiOutlinedInput-notchedOutline":
-  //     {
-  //       color: `${fieldColor}`,
-  //       borderColor: `${fieldColor}`,
-  //     },
+  // Utility function to dynamically access the theme color using object path string
+  const getColorFromTheme = (path: string) => {
+    const keys = path.split("."); // Split the path like "primary.main" into ["primary", "main"]
+    let color: Palette | string = theme.palette; 
+    keys.forEach((key) => {
+      // Here's how we'll loop through the palette object
+      // theme.palette["primary"]["main"]
+      color = color[key as keyof typeof color]; 
+    });
+    return color; 
+  };
 
-// second way of doing it, more detailed
-  "& .MuiInputLabel-root": {
-    color: fieldColor,
-  },
-  "& .MuiInputLabel-root.Mui-focused": {
-    color: fieldColor,
-  },
-  "& .MuiInputBase-input": {
-    color: fieldColor,
-  },
-  "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-    borderColor: fieldColor,
-  },
-  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-    borderColor: fieldColor,
-  },
-  "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-    borderColor: fieldColor,
-  },
-  "& .MuiInputBase-input::placeholder": {
-    color: fieldColor,
-  },
-  "& .MuiFormHelperText-root": {
-    color: fieldColor,
-  },
-  "& .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline": {
-    borderColor: fieldColor,
-  },
-  "& .MuiInputLabel-root.Mui-error": {
-    color: fieldColor,
-  },
-  "& .MuiFormHelperText-root.Mui-error": {
-    color: fieldColor,
-  },
-  "& .MuiInputBase-root.Mui-disabled": {
-    color: fieldColor,
-    backgroundColor: fieldColor,
-  },
-  "& .MuiInputAdornment-root": {
-    color: fieldColor,
-  },
-  "& .MuiOutlinedInput-root.Mui-disabled .MuiOutlinedInput-notchedOutline": {
-    borderColor: fieldColor,
-  },
-}));
+  const colorFromTheme = fieldColor
+    ? getColorFromTheme(fieldColor)
+    : theme.palette.primary.main;
+
+  return {
+    /*
+        * .MuiInputLabel-root => style of the label of the text field.
+        * .MuiInputLabel-root.Mui-focused => Controls the label's color when the input field is focused.
+        * .MuiInputBase-input => Controls the color and style of the actual text entered inside the input field.
+        * .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline => Controls the outline color and style when the text field is in its normal state.
+        * .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline => Controls the outline color when the input field is focused.
+        * .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline => Controls the outline color when hovering over the input field.    
+        * .MuiInputBase-input::placeholder => Controls the style and color of the placeholder text inside the input field.
+        * .MuiFormHelperText-root => Controls the color and style of the helper text displayed below the input field.
+        * .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline => Controls the outline color when the input field has an error.
+        * .MuiInputLabel-root.Mui-error => Controls the color of the label when the input field is in an error state.
+        * .MuiFormHelperText-root.Mui-error => Controls the color of the helper text when there's an error.
+        * .MuiInputBase-root.Mui-disabled => Controls the styles when the input is disabled.
+        
+        */
+
+    // first way of doing it, more compact
+    //   ".MuiInputLabel-root, .MuiInputLabel-root.Mui-focused, .MuiInputBase-input, .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline, .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline, .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline, .MuiInputBase-input::placeholder, .MuiFormHelperText-root, .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline, .MuiInputLabel-root.Mui-error, .MuiFormHelperText-root.Mui-error, .MuiInputBase-root.Mui-disabled,.MuiInputAdornment-root, .MuiOutlinedInput-root.Mui-disabled .MuiOutlinedInput-notchedOutline":
+    //     {
+    //       color: `${fieldColor}`,
+    //       borderColor: `${fieldColor}`,
+    //     },
+
+    // second way of doing it, more detailed
+    "& .MuiInputLabel-root": {
+      color: colorFromTheme,
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: colorFromTheme,
+    },
+    "& .MuiInputBase-input": {
+      color: colorFromTheme,
+    },
+    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: colorFromTheme,
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: colorFromTheme,
+    },
+    "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+      borderColor: colorFromTheme,
+    },
+    "& .MuiInputBase-input::placeholder": {
+      color: colorFromTheme,
+    },
+    "& .MuiFormHelperText-root": {
+      color: colorFromTheme,
+    },
+    "& .MuiOutlinedInput-root.Mui-error .MuiOutlinedInput-notchedOutline": {
+      borderColor: colorFromTheme,
+    },
+    "& .MuiInputLabel-root.Mui-error": {
+      color: colorFromTheme,
+    },
+    "& .MuiFormHelperText-root.Mui-error": {
+      color: colorFromTheme,
+    },
+    "& .MuiInputBase-root.Mui-disabled": {
+      color: colorFromTheme,
+      backgroundColor: colorFromTheme,
+    },
+    "& .MuiInputAdornment-root": {
+      color: colorFromTheme,
+    },
+    "& .MuiOutlinedInput-root.Mui-disabled .MuiOutlinedInput-notchedOutline": {
+      borderColor: colorFromTheme,
+    },
+  };
+});
 
 export const CInputField = ({
   name,
   label,
   type,
   variant = "outlined",
-  fieldColor = "#fff",
+  fieldColor = "primary.main",
+  // fieldColor = "#fff",
   ...restProps
 }: CustomInputFieldTypes) => {
   return (
