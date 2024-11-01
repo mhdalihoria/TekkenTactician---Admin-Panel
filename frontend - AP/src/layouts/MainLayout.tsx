369ca44708892cebfef7ec42b37ab6604b3e575a
mhdalihoria <alihoria123456@gmail.com>
2024-11-01 14:02:08 +0200

    Adding Outlet

--- frontend - AP/src/layouts/MainLayout.tsx
+++ frontend - AP/src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Box, styled } from "@mui/material";
 import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const LayoutContainer = styled(Box)(({ theme }) => ({
   minWidth: "100vw",
@@ -10,7 +11,9 @@ export default function MainLayout() {
   return (
     <LayoutContainer>
       <Navbar />
-      <div>MainLayout</div>
+      <Box sx={{margin: "0 1.5rem"}}>
+        <Outlet />
+      </Box>
     </LayoutContainer>
   );
 }
