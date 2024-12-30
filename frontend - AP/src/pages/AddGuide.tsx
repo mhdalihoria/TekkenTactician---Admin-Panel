d9b8c08a5a6b11f045a5163fc83a6b6bfdf870b3
mhdalihoria <alihoria123456@gmail.com>
2024-12-30 19:18:52 +0300

    Basic version of "add guide" page

--- frontend - AP/src/pages/AddGuide.tsx
+++ frontend - AP/src/pages/AddGuide.tsx
@@ -1,6 +1,36 @@
+import { CInputField } from "../custom-components/form/CInputField";
+import MultiInputField from "../custom-components/form/MultiInputField";
+import TextEditor from "../custom-components/form/TextEditor";
+const array = [
+  {
+    startup: "i13",
+    move: "b1+2",
+  },
+  {
+    startup: "i12",
+    move: "1+2",
+  },
+];
 
 export default function AddGuide() {
   return (
-    <div>AddGuide</div>
-  )
+    <div>
+      <h1 style={{ color: "white" }}>single field</h1>
+      <CInputField
+        name="title"
+        label="Title"
+        placeholder="Character Title"
+        fieldColor="secondary.main"
+      />
+
+      <h1 style={{ color: "white" }}>multiple field</h1>
+      <MultiInputField fields={array} />
+
+      <h1 style={{ color: "white" }}>Text Editor</h1>
+      <TextEditor
+        value="<initial_value>"
+        onChange={(newContent) => console.log("Content:", newContent)}
+      />
+    </div>
+  );
 }
