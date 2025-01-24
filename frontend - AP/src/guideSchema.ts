107a2f77b92c6fc2b2d6b156a14474f3c43b9752
mhdalihoria <alihoria123456@gmail.com>
2025-01-24 14:49:05 +0300

    Recovering OG Char schema

--- frontend - AP/src/guideSchema.ts
+++ frontend - AP/src/guideSchema.ts
@@ -1,229 +1,585 @@
-const guideSchema = {
-  guide: {
-    name: {
-      controlType: "input-field",
-      value: "",
-      label: "Name",
-      placeholder: "Character Name",
-    },
-    picUrl: {
-      controlType: "input-field",
-      value: "",
-      label: "Pic Url",
-      placeholder: "Character Pic URL",
-    },
-    heatEngagers: {
-      controlType: "array",
-      label: "Heat Engagers",
-      items: [],
-      template: {
-        move: {
-          controlType: "input-field",
-          value: "",
-          label: "Move",
-          placeholder: "1+2",
-        },
-        description: {
-          controlType: "input-field",
-          value: "",
-          label: "Description",
-          placeholder: "Kinda op ngl",
-        },
+{
+  "_id": "661d95ad6f6ae8dacffe1b3b",
+  "wallCombos": {
+    "general": [
+      {
+        "move": "1 2",
+        "followUp": "f 3",
+        "_id": "661d95ad6f6ea8dacffe1b5b"
       },
-    },
-    punishers: {
-      controlType: "array",
-      label: "Punishers",
-      items: [],
-      template: {
-        startup: {
-          controlType: "input-field",
-          value: "",
-          label: "Startup",
-          placeholder: "i13, i15, etc...",
-        },
-        move: {
-          controlType: "input-field",
-          value: "",
-          label: "Move",
-          placeholder: "1+2",
-        },
+      {
+        "move": "df 4 3 4 ~B",
+        "followUp": "1+4",
+        "_id": "661d95ad6f6ea8dacffe1b5c"
+      }
+    ],
+    "withTornado": [
+      {
+        "move": "b 1 2",
+        "followUp": "f f f 2+4",
+        "_id": "661d95ad6f6ea8dacffe1b5d"
       },
-    },
-    followup: {
-      controlType: "array",
-      label: "Follow-ups",
-      items: [],
-      template: {
-        move: {
-          controlType: "input-field",
-          value: "",
-          label: "Follow-up Move",
-          placeholder: "Follow-up Move",
-        },
-        followup: {
-          controlType: "input-field",
-          value: "",
-          label: "Follow-up Description",
-          placeholder: "Follow-up Description",
-        },
-      },
-    },
-    grabs: {
-      controlType: "array",
-      label: "Grabs",
-      items: [],
-      template: {
-        grab: {
-          controlType: "input-field",
-          value: "",
-          label: "Grab",
-          placeholder: "Grab",
-        },
-        break: {
-          controlType: "input-field",
-          value: "",
-          label: "Break",
-          placeholder: "Break",
-        },
-      },
-    },
-    combos: {
-      controlType: "array",
-      label: "Combos",
-      items: [],
-      template: {
-        title: {
-          controlType: "input-field",
-          value: "",
-          label: "Title",
-          placeholder: "Combo Title",
-        },
-        launchers: {
-          controlType: "array",
-          label: "Launchers",
-          items: [],
-          template: {
-            controlType: "input-field",
-            value: "",
-            label: "Launcher",
-            placeholder: "Launcher",
-          },
-        },
-        combo: {
-          controlType: "text-editor",
-          value: "",
-        },
-        simpleVersion: {
-          controlType: "text-editor",
-          value: "",
-        },
-        demoUrl: {
-          controlType: "input-field",
-          value: "",
-          label: "Demo URL",
-          placeholder: "Demo URL",
-        },
-      },
-    },
-    comboEnders: {
-      dmg: {
-        controlType: "array",
-        label: "Damage Enders",
-        items: [],
-        template: {
-          controlType: "input-field",
-          value: "",
-          label: "Damage Ender",
-          placeholder: "Ender for Damage",
-        },
-      },
-      wallbrk: {
-        controlType: "array",
-        label: "Wall Break Enders",
-        items: [],
-        template: {
-          controlType: "input-field",
-          value: "",
-          label: "Wall Break Ender",
-          placeholder: "Ender for Wall Break",
-        },
-      },
-      flrbrk: {
-        controlType: "array",
-        label: "Floor Break Enders",
-        items: [],
-        template: {
-          controlType: "input-field",
-          value: "",
-          label: "Floor Break Ender",
-          placeholder: "Ender for Floor Break",
-        },
-      },
-    },
-    chainGrabs: {
-      controlType: "text-editor",
-      value: "",
-    },
-    creatorNotes: {
-      controlType: "text-editor",
-      value: "",
-    },
+      {
+        "move": "f f 4",
+        "followUp": "f f f 2+4",
+        "_id": "661d95ad6f6ea8dacffe1b5e"
+      }
+    ]
   },
-  anti: {
-    overview: {
-      controlType: "text-editor",
-      value: "",
+  "name": "King II",
+  "image": "https://drive.google.com/thumbnail?id=1EQ07JO_Lmv98tpiohdGjNQedbm8N1mmv&sz=w1000",
+  "mostImportantGrabs": [
+    {
+      "move": "f  hcf  1",
+      "escape": "1",
+      "_id": "661d95ad6f6ea8dacffe1b3c"
     },
-    counterStrategy: {
-      controlType: "text-editor",
-      value: "",
+    {
+      "move": "f  hcf  2",
+      "escape": "2",
+      "_id": "661d95ad6f6ea8dacffe1b3d"
     },
-    weakSide: {
-      controlType: "input-field",
-      value: "",
-      label: "Weak Side",
-      placeholder: "Weak Side",
+    {
+      "move": "f  f  f  2+4",
+      "escape": "1+2",
+      "_id": "661d95ad6f6ea8dacffe1b3e"
     },
-    range: {
-      controlType: "input-field",
-      value: "",
-      label: "Preferred Range",
-      placeholder: "e.g., Close Range",
+    {
+      "move": "qcb  1+2",
+      "escape": "1+2",
+      "_id": "661d95ad6f6ea8dacffe1b3f"
     },
-    keyMoves: {
-      controlType: "text-editor",
-      value: "",
+    {
+      "move": "uf  1+2",
+      "escape": "1+2",
+      "_id": "661d95ad6f6ea8dacffe1b40"
+    }
+  ],
+  "guaranteedFollowUps": [
+    {
+      "move": "f  f  2",
+      "followUp": "d 1+3  -  d 2+4  -  d 1+4",
+      "_id": "661d95ad6f6ea8dacffe1b41"
     },
-    detailedStrategies: {
-      controlType: "array",
-      label: "Detailed Strategies",
-      items: [],
-      template: {
-        title: {
-          controlType: "input-field",
-          value: "",
-          label: "Title",
-        },
-        counterStrategy: {
-          controlType: "text-editor",
-          value: "",
-        },
-        moves: {
-          controlType: "text-editor",
-          value: "",
-        },
+    {
+      "_id": "678b9627586b3ba2ad765fa5",
+      "move": "WR 3",
+      "followUp": "f f 4  -  f 4 BigChars - f 2+3 Inconsistent"
+    },
+    {
+      "move": "CH  1 2 1",
+      "followUp": "2 1",
+      "_id": "661d95ad6f6ea8dacffe1b42"
+    },
+    {
+      "move": "CH  4",
+      "followUp": "d 2+3",
+      "_id": "661d95ad6f6ea8dacffe1b43"
+    },
+    {
+      "move": "CH  db 3",
+      "followUp": "d 2+3",
+      "_id": "661d95ad6f6ea8dacffe1b44"
+    },
+    {
+      "move": "CH  f  f 1",
+      "followUp": "d 2+3",
+      "_id": "661d95ad6f6ea8dacffe1b45"
+    },
+    {
+      "move": "CH  f  f  n  1+2",
+      "followUp": "f 2 1  -  b 3 1+2",
+      "_id": "661d95ad6f6ea8dacffe1b46"
+    }
+  ],
+  "heatEngagers": [
+    {
+      "move": "f 2 1",
+      "description": "Great whiff punisher. Launches as a Heat Dash",
+      "_id": "661d95ad6f6ea8dacffe1b47"
+    },
+    {
+      "move": "f 2+3",
+      "description": "Safe mid Heat Engager with good range",
+      "_id": "661d95ad6f6ea8dacffe1b48"
+    },
+    {
+      "move": "db 1+2 2",
+      "description": "Safe mid Heat Engager",
+      "_id": "661d95ad6f6ea8dacffe1b49"
+    },
+    {
+      "move": "JGR( f 3+4 )  1+3  2+4",
+      "description": "Unescapable throw that goes into Heat",
+      "_id": "661d95ad6f6ea8dacffe1b4a"
+    },
+    {
+      "move": "d 1+4",
+      "description": "Useful to punish crouching opponents",
+      "_id": "661d95ad6f6ea8dacffe1b4b"
+    }
+  ],
+  "importantMoves": [],
+  "punishers": {
+    "startup": [
+      {
+        "move": "2 1",
+        "frames": "10",
+        "_id": "661fc14cb68759501c824fff"
       },
-    },
-    antiDrills: {
-      controlType: "text-editor",
-      value: "",
-    },
-    creatorNotes: {
-      controlType: "text-editor",
-      value: "",
-    },
+      {
+        "move": "WS 4",
+        "frames": "11",
+        "_id": "661fc14cb68759501c825000"
+      },
+      {
+        "move": "b 1 2",
+        "frames": "12",
+        "_id": "661fc14cb68759501c825001"
+      },
+      {
+        "move": "b 2 1",
+        "frames": "14",
+        "_id": "661fc14cb68759501c825002"
+      },
+      {
+        "move": "WS 2 2",
+        "frames": "14",
+        "_id": "661fc14cb68759501c825003"
+      },
+      {
+        "move": "uf 4",
+        "frames": "15",
+        "_id": "661fc14cb68759501c825004"
+      },
+      {
+        "move": "f 2 1",
+        "frames": "15",
+        "_id": "661fc14cb68759501c825005"
+      },
+      {
+        "move": "FC  df 2",
+        "frames": "15",
+        "_id": "661fc14cb68759501c825006"
+      },
+      {
+        "move": "WS 1+2",
+        "frames": "18",
+        "_id": "661fc14cb68759501c825007"
+      }
+    ],
+    "_id": "661fc14cb68759501c824ffe"
   },
-};
-
-export default guideSchema;
+  "importantCombos": [
+    {
+      "launchers": [
+        "uf 4",
+        "ws 1+2"
+      ],
+      "followUps": [
+        "4",
+        "b 2 4",
+        "b 3",
+        "T",
+        "2 WR 2+4"
+      ],
+      "followUpSimple": [
+        "f 3",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "_id": "661d95ad6f6ea8dacffe1b53",
+      "vidUrl": "https://res.cloudinary.com/dlkgqs21x/video/upload/v1714484161/tekkentactician/combo%20vids/TEKKEN_8_20240430144732_cytprn.webm"
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fba",
+      "launchers": [
+        "alt-wall-carry",
+        "uf 4",
+        "ws 1+2"
+      ],
+      "followUps": [
+        "4",
+        "mini-dash",
+        "df 4 3",
+        "1 2",
+        "uf 3+4 ~B",
+        "T",
+        "2 wr 2+4"
+      ],
+      "followUpSimple": [
+        "df 4 3",
+        "1 2",
+        "uf 3+4",
+        "T",
+        "b 1 2"
+      ],
+      "vidUrl": "https://res.cloudinary.com/dlkgqs21x/video/upload/v1714484176/tekkentactician/combo%20vids/TEKKEN_8_20240430145055_r00ivu.webm"
+    },
+    {
+      "launchers": [
+        "FC  df  2"
+      ],
+      "followUps": [
+        "f f 1",
+        "df 4 3",
+        "1 2",
+        "uf 3+4 ~B",
+        "T",
+        "2 WR : 2+4"
+      ],
+      "followUpSimple": [
+        "f f  1",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "_id": "661d95ad6f6ea8dacffe1b54",
+      "vidUrl": "https://res.cloudinary.com/dlkgqs21x/video/upload/v1714571272/tekkentactician/combo%20vids/TEKKEN_8_20240501145920_ot8bcv.webm"
+    },
+    {
+      "launchers": [
+        "JGS 1"
+      ],
+      "followUps": [
+        "SSR",
+        "f 3",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "followUpSimple": [],
+      "_id": "661d95ad6f6ea8dacffe1b55",
+      "endTime": 17,
+      "vidUrl": "https://youtu.be/CMOjbNKK2NE?feature=shared&t=28"
+    },
+    {
+      "launchers": [
+        "db 2",
+        "u 1+2"
+      ],
+      "followUps": [
+        "f 3",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "followUpSimple": [],
+      "_id": "661d95ad6f6ea8dacffe1b56"
+    },
+    {
+      "launchers": [
+        "FC  df 1",
+        "low parry"
+      ],
+      "followUps": [
+        "f f 1",
+        "df 4 3",
+        "2",
+        "WR : 2+4"
+      ],
+      "followUpSimple": [
+        "WR 2+4"
+      ],
+      "_id": "661d95ad6f6ea8dacffe1b57",
+      "vidUrl": "https://youtu.be/CMOjbNKK2NE?feature=shared&t=45",
+      "endTime": 7
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fbf",
+      "launchers": [
+        "only-vs-big-chars",
+        "CH df 2 1"
+      ],
+      "followUps": [
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "SSL",
+        "2",
+        "WR : 2+4"
+      ],
+      "followUpSimple": [
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "f 2 1"
+      ]
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fc0",
+      "followUps": [
+        "d 3+4",
+        "d 3+4",
+        "d 3+4",
+        "2"
+      ],
+      "followUpSimple": [],
+      "launchers": [
+        "CH df 2 1"
+      ]
+    },
+    {
+      "launchers": [
+        "CH  b 1 2"
+      ],
+      "followUps": [
+        "df 4 3",
+        "2",
+        "WR : 2+4"
+      ],
+      "followUpSimple": [
+        "WR 2+4"
+      ],
+      "_id": "661d95ad6f6ea8dacffe1b58",
+      "endTime": 12,
+      "vidUrl": "https://youtu.be/CMOjbNKK2NE?feature=shared&t=53"
+    },
+    {
+      "launchers": [
+        "CH  f n d df 4",
+        "CH  f n d df 1+2"
+      ],
+      "followUps": [
+        "f 3",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "followUpSimple": [],
+      "_id": "661d95ad6f6ea8dacffe1b59",
+      "endTime": 23,
+      "vidUrl": "https://youtu.be/CMOjbNKK2NE?feature=shared&t=65"
+    },
+    {
+      "launchers": [
+        "WHILE-IN-HEAT",
+        "f 2 1  ~F heat",
+        "f 2+3  ~F heat",
+        "db 1+2 2 ~F heat"
+      ],
+      "followUps": [
+        "f 3",
+        "f 3",
+        "f 3",
+        "df 4 3",
+        "b 1 2",
+        "T",
+        "WR 2+4"
+      ],
+      "followUpSimple": [],
+      "_id": "661d95ad6f6ea8dacffe1b5a",
+      "vidUrl": "https://youtu.be/CMOjbNKK2NE?feature=shared&t=89"
+    }
+  ],
+  "comboEnders": [
+    {
+      "move": "f  f  f 2+4",
+      "category": "for dmg",
+      "_id": "661d95ad6f6ea8dacffe1b5f"
+    },
+    {
+      "move": "f 2 1",
+      "category": "wall carry",
+      "_id": "661d95ad6f6ea8dacffe1b60"
+    },
+    {
+      "move": "df 4 3",
+      "category": "wall carry",
+      "_id": "661d95ad6f6ea8dacffe1b61"
+    },
+    {
+      "move": "f 2 4",
+      "category": "wall carry",
+      "_id": "661d95ad6f6ea8dacffe1b62"
+    },
+    {
+      "move": "b 1 2",
+      "category": "wall carry",
+      "_id": "661d95ad6f6ea8dacffe1b63"
+    },
+    {
+      "move": "f 2 d 1 2",
+      "category": "wall carry",
+      "_id": "661d95ad6f6ea8dacffe1b64"
+    },
+    {
+      "move": "f 2 1",
+      "category": "wall break",
+      "_id": "661d95ad6f6ea8dacffe1b65"
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fcf",
+      "move": "f 2+3",
+      "category": "wall break"
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fd0",
+      "move": "f f 1",
+      "category": "wall break"
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fd1",
+      "move": "b 2 2",
+      "category": "wall break"
+    },
+    {
+      "move": "f n d df 1+2",
+      "category": "floor break",
+      "_id": "661d95ad6f6ea8dacffe1b66"
+    },
+    {
+      "move": "f f 2",
+      "category": "floor break",
+      "_id": "661d95ad6f6ea8dacffe1b67"
+    },
+    {
+      "move": "f f f 2+4",
+      "category": "floor break",
+      "_id": "661d95ad6f6ea8dacffe1b68"
+    },
+    {
+      "_id": "678b9627586b3ba2ad765fd5",
+      "move": "db 2",
+      "category": "floor break"
+    }
+  ],
+  "__v": 0,
+  "heatSystem": {
+    "ender": "WR 3+4",
+    "engager": "f 2 1",
+    "_id": "678b9627586b3ba2ad765fd6"
+  },
+  "chainThrows": [
+    {
+      "name": "Rolling Death Cradle - RDC",
+      "throw": [
+        "f n d ~DF 1+4"
+      ],
+      "throwBreak": [
+        "1"
+      ],
+      "followUp": [
+        "2 1 1+3",
+        "1 3 4 2 1+2"
+      ],
+      "followUpBreak": [
+        "hold 1+2",
+        "hold 2"
+      ],
+      "simpleInput": [
+        "2 1 mash 1+2+3+4",
+        "4 2 mash 1+2+3+4"
+      ],
+      "simpleBreak": [
+        "hold 1+2",
+        "hold 2"
+      ],
+      "_id": "6630c5ca759d09a71eec621c"
+    },
+    {
+      "name": "Cobra twist into Screwdriver",
+      "throw": [
+        "Jaguar Step 1+4"
+      ],
+      "throwBreak": [
+        "1"
+      ],
+      "followUp": [
+        "1 4 2 3",
+        "1 1+3",
+        "1+2 1 1+3",
+        "2 3 1 1+2",
+        "3+4 1+2",
+        "1+2 3 1+3",
+        "2 4 3 1 1+3"
+      ],
+      "followUpBreak": [
+        "unbreakable",
+        "1",
+        "1",
+        "1+2",
+        "unbreakable",
+        "1",
+        "1"
+      ],
+      "simpleInput": [
+        "mash 1+2+3+4"
+      ],
+      "simpleBreak": [
+        "hold 1"
+      ],
+      "_id": "6630dde77aaf6b1088953729"
+    },
+    {
+      "name": "Reverse Special Stretch Bomb Combo into Giant Swing",
+      "throw": [
+        "df 2+4",
+        "SS 2+4"
+      ],
+      "throwBreak": [
+        "2",
+        "2"
+      ],
+      "followUp": [
+        "1 3+4 1",
+        "1 2 3+4 1+2",
+        "1 2 1 3 4",
+        "1"
+      ],
+      "followUpBreak": [
+        "unbreakable",
+        "1",
+        "unbreakable",
+        "1"
+      ],
+      "simpleInput": [
+        "mash 1+2+3+4"
+      ],
+      "simpleBreak": [
+        "hold 1"
+      ],
+      "_id": "6630e1a17aaf6b10889542e5"
+    },
+    {
+      "name": "Reverse Special Stretch Bomb Combo into Muscle Buster",
+      "throw": [
+        "df 1+3",
+        "SS 1+3"
+      ],
+      "throwBreak": [
+        "2",
+        "2"
+      ],
+      "followUp": [
+        "2 3+4 2",
+        "2 1 2 3+4",
+        "2 3 1 2 3+4 1+2",
+        "2"
+      ],
+      "followUpBreak": [
+        "unbreakable",
+        "2",
+        "unbreakable",
+        "2"
+      ],
+      "simpleInput": [
+        "mash 1+2+3+4",
+        "mash 2 after 3rd hit"
+      ],
+      "simpleBreak": [
+        "hold 1",
+        "hold 2"
+      ],
+      "_id": "6630e3a07aaf6b10889542e7"
+    }
+  ],
+  "counterSchema": [],
+  "creatorNotes": []
+}
